2b43251b010894a4d5a9a8ea74f3c1434e7cd96d
rimaru06 <shivraodanger04@gmail.com>
2024-08-29 15:56:24 +0530

    docker compose updated

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,10 @@
 import prisma from '../src/db';
-import fs  from 'fs';
+import fs from 'fs';
 import path from 'path';
 
 const filePath = path.join(__dirname, 'data.json');
 
-async function seedSubjects()
-{
+async function seedSubjects() {
     try {
         await prisma.subject.upsert({
             where: { subject_code: "EE-101" },
@@ -37,77 +36,77 @@ async function seedSubjects()
             update: {}
         })
     } catch (error) {
-    console.error('Error seeding users:', error);
-    throw error;
+        console.error('Error seeding users:', error);
+        throw error;
     }
 }
 
 async function seedData() {
-    const data = fs.readFileSync(filePath , 'utf-8');
+    const data = fs.readFileSync(filePath, 'utf-8');
     const students = JSON.parse(data);
 
     try {
-        const sub_ids =  await prisma.subject.findMany({
-            select : {
-                subject_id : true,
-                subject_name : true
+        const sub_ids = await prisma.subject.findMany({
+            select: {
+                subject_id: true,
+                subject_name: true
             }
         })
-        const sub_id_map = new Map(sub_ids.map((sub: any) => [sub.subject_name , sub.subject_id]));
+        const sub_id_map = new Map(sub_ids.map((sub: any) => [sub.subject_name, sub.subject_id]));
 
-    interface UserAttendance {
-        roll_Number: string;
-        group: string;
-        subjectid: string;
-        present_Days?: number;
-        current_classes?: number;
-        absent_Days?: number;
-    }
+        interface UserAttendance {
+            roll_Number: string;
+            group: string;
+            subjectid: string;
+            present_Days?: number;
+            current_classes?: number;
+            absent_Days?: number;
+        }
 
-    interface Student {
-        roll_number: string;
-        group_name: string;
-        subject_name: string;
-        date: Date;
-        present: boolean;
-    }
-    const userAttendances: UserAttendance[] = students.map((student: Student) => ({
-        roll_Number: student.roll_number,
-        group: student.group_name,
-        subjectid: sub_id_map.get(student.subject_name),
-        present_Days : 30,
-        current_classes : 50,
-        absent_Days : 20
-    }));
+        interface Student {
+            roll_number: string;
+            group_name: string;
+            subject_name: string;
+            date: Date;
+            present: boolean;
+        }
+        const userAttendances: UserAttendance[] = students.map((student: Student) => ({
+            roll_Number: student.roll_number,
+            group: student.group_name,
+            subjectid: sub_id_map.get(student.subject_name),
+            present_Days: 30,
+            current_classes: 50,
+            absent_Days: 20
+        }));
+        console.log(userAttendances.length);
 
-    await prisma.userAttendance.createMany({
-        data: userAttendances,
-    })
+        await prisma.userAttendance.createMany({
+            data: userAttendances,
+        })
 
-   interface Attendance {
-        roll_Number: string;
-        subject_id: string;
-        present : boolean;
-        date : Date;
-   } 
+        interface Attendance {
+            roll_Number: string;
+            subject_id: string;
+            present: boolean;
+            date: Date;
+        }
 
-   const attendances: Attendance[] = students.map((student : Student) => ({
-    roll_Number: student.roll_number,
-    subject_id: sub_id_map.get(student.subject_name),
-    present: student.present,
-    date: student.date
-}));
+        const attendances: Attendance[] = students.map((student: Student) => ({
+            roll_Number: student.roll_number,
+            subject_id: sub_id_map.get(student.subject_name),
+            present: student.present,
+            date: student.date
+        }));
 
-   await prisma.attendance.createMany({
-    data : attendances
-   })
+        await prisma.attendance.createMany({
+            data: attendances
+        })
     } catch (error) {
         throw error;
     }
 }
 
-async function seedDatabase()
-{
+async function seedDatabase() {
     try {
         await seedSubjects();
         await seedData();
@@ -115,7 +114,7 @@ async function seedDatabase()
         console.error('Error seeding database:', error);
         throw error;
     } finally {
-      await  prisma.$disconnect();
+        await prisma.$disconnect();
     }
 }
 
